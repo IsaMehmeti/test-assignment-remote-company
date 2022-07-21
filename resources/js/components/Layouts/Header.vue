7427ccb7dffed22f26d50acf7a3205f54835c2bb
Isa <isa@aboutbirdie.com>
2022-07-21 13:02:10 +0200

    Edit Navbar

--- resources/js/components/Layouts/Header.vue
+++ resources/js/components/Layouts/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <b-navbar type="is-light" spaced>
+      <b-navbar type="is-light" spaced >
           <template #brand>
             <b-navbar-item>
                 <img
@@ -10,11 +10,19 @@
             </b-navbar-item>
         </template>
         <template #start>
-            <b-navbar-item href="/home" :active="activePage === 'Home'">
-                Home
+            <b-navbar-item  href="/home" :active="activePage === 'Home'">
+              <b-icon
+                icon="home"
+                class="mr-1"
+              >
+            </b-icon>Home
             </b-navbar-item>
             <b-navbar-item href="/admin" :active="activePage === 'Admin'">
-                Admin
+                <b-icon
+                icon="account"
+                class="mr-2"
+              >
+            </b-icon>Admin
             </b-navbar-item>
         </template>
     </b-navbar>
